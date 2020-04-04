89b96b556d6956260f3bea7f16bab19594b2f90c
Dostoyewski <dostoyewski@yandex.ru>
2020-04-04 13:26:25 +0300

    Updated serializers classes

--- API/serializers.py
+++ API/serializers.py
@@ -10,6 +10,23 @@ class UserDetailSerializer(serializers.ModelSerializer):
         model = User
         fields = '__all__'
 
+    def create(self, validated_data):
+        """
+        Create a new `User` instance
+        :param validated_data: request data
+        :return: new `User` instance
+        """
+        return User.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update current `User` instance
+        :param instance: existing `User` instance
+        :param validated_data: request data
+        :return: updated `User` instance
+        """
+        # TODO: Write fields setters
+
 
 class HeroDetailSerializer(serializers.ModelSerializer):
     """
@@ -19,6 +36,23 @@ class HeroDetailSerializer(serializers.ModelSerializer):
         model = Hero
         fields = '__all__'
 
+    def create(self, validated_data):
+        """
+        Create a new `Hero` instance
+        :param validated_data: request data
+        :return: new `Hero` instance
+        """
+        return Hero.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update current `Hero` instance
+        :param instance: existing `Hero` instance
+        :param validated_data: request data
+        :return: updated `Hero` instance
+        """
+        # TODO: Write fields setters
+
 
 class PhotoDetailSerializer(serializers.ModelSerializer):
     """
@@ -26,6 +60,21 @@ class PhotoDetailSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = Photo
-        fields = '__all__'       
+        fields = '__all__'
 
+    def create(self, validated_data):
+        """
+        Create a new `Photo` instance
+        :param validated_data: request data
+        :return: new `Photo` instance
+        """
+        return User.objects.create(**validated_data)
 
+    def update(self, instance, validated_data):
+        """
+        Update current `Photo` instance
+        :param instance: existing `Photo` instance
+        :param validated_data: request data
+        :return: updated `Photo` instance
+        """
+        # TODO: Write fields setters
